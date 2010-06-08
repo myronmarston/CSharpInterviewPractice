4ebc789de654af41c4c6bbe1ec0fdc204ae8f2fc
Myron Marston <myron.marston@gmail.com>
2010-06-08 13:32:30 -0700

    Added basic TreeNode Class.

---
+++ TreeNode.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CSharpInterviewPractice
+{
+    public class TreeNode<T> where T : IComparable
+    {
+        public TreeNode(T value)
+        {
+            this.value = value;
+        }
+
+        public TreeNode(T value, TreeNode<T> parent)
+            : this(value)
+        {
+            this.parent = parent;
+        }
+
+        private T value;
+        private TreeNode<T> parent;
+        private IList<TreeNode<T>> children;
+
+        public T Value { get { return value; } } 
+        public TreeNode<T> Parent { get { return parent;  } }
+        public IList<TreeNode<T>> Children { get { if (children == null) children = new List<TreeNode<T>>(); return children; } }
+
+        public TreeNode(TreeNode<T> parent)
+        {
+            this.parent = parent;
+        }
+
+        public bool IsRoot { get { return (Parent == null); } }
+        public bool IsLeaf { get { return (Children.Count == 0); } }
+
+        public int Height
+        {
+            get
+            {
+                if (IsLeaf) return 0;
+                return Children.Max((TreeNode<T> node) => node.Height) + 1;
+            }
+        }
+
+        public int Depth
+        {
+            get 
+            {
+                if (IsRoot) return 0;
+                return Parent.Depth + 1;
+            }
+        }
+
+        public IList<TreeNode<T>> Siblings
+        {
+            get
+            {
+                if (this.Parent == null) return new List<TreeNode<T>>();
+                return new List<TreeNode<T>>(this.Parent.Children.Except(new TreeNode<T>[] { this }));
+            }
+        }
+
+        public TreeNode<T> AddChild(T value)
+        {
+            TreeNode<T> child = new TreeNode<T>(value, this);
+            this.Children.Add(child);
+            return child;
+        }
+    }
+
+    [TestFixture]
+    class TreeNodeTest
+    {
+        private TreeNode<int> ExampleTreeRoot()
+        {
+            TreeNode<int> root = new TreeNode<int>(7);
+            TreeNode<int> three = root.AddChild(3);
+            three.AddChild(2);
+            three.AddChild(5);
+            three.AddChild(12);
+
+            TreeNode<int> four = root.AddChild(4);
+            TreeNode<int> nine = four.AddChild(9);
+            nine.AddChild(6);
+
+            return root;
+        }
+
+        [Test]
+        public void IsRootReturnsExpectedValue()
+        {
+            TreeNode<int> root = this.ExampleTreeRoot();
+            Assert.IsTrue(root.IsRoot);
+
+            Assert.IsFalse(root.Children[0].IsRoot);
+            Assert.IsFalse(root.Children[1].IsRoot);
+            Assert.IsFalse(root.Children[0].Children[0].IsRoot);
+        }
+
+        [Test]
+        public void IsLeafReturnsExpectedValue()
+        {
+            TreeNode<int> root = this.ExampleTreeRoot();
+            Assert.IsFalse(root.IsLeaf);
+
+            Assert.IsFalse(root.Children[0].IsLeaf);
+            Assert.IsFalse(root.Children[1].IsLeaf);
+            Assert.IsTrue(root.Children[0].Children[0].IsLeaf);
+        }
+
+        [Test]
+        public void DepthReturnsExpectedValue()
+        {
+            TreeNode<int> root = this.ExampleTreeRoot();
+            Assert.AreEqual(0, root.Depth);
+            Assert.AreEqual(1, root.Children[0].Depth);
+            Assert.AreEqual(1, root.Children[1].Depth);
+
+            Assert.AreEqual(2, root.Children[0].Children[1].Depth);
+            Assert.AreEqual(3, root.Children[1].Children[0].Children[0].Depth);
+        }
+
+        [Test]
+        public void HeightReturnsExpectedValue()
+        {
+            TreeNode<int> root = this.ExampleTreeRoot();
+            Assert.AreEqual(3, root.Height);
+            Assert.AreEqual(1, root.Children[0].Height);
+            Assert.AreEqual(2, root.Children[1].Height);
+
+            Assert.AreEqual(0, root.Children[0].Children[1].Height);
+
+            Assert.AreEqual(1, root.Children[1].Children[0].Height);
+            Assert.AreEqual(0, root.Children[1].Children[0].Children[0].Height);
+        }
+
+        [Test]
+        public void SiblingsReturnsExpectedValues()
+        {
+            TreeNode<int> root = this.ExampleTreeRoot();
+            Assert.AreEqual(0, root.Siblings.Count());
+
+            Assert.AreEqual(3, root.Children[0].Value);
+            Assert.AreEqual(1, root.Children[0].Siblings.Count());
+            Assert.AreEqual(4, root.Children[0].Siblings[0].Value);
+
+            Assert.AreEqual(2, root.Children[0].Children[0].Value);
+            Assert.AreEqual(2, root.Children[0].Children[0].Siblings.Count);
+            Assert.AreEqual(5, root.Children[0].Children[0].Siblings[0].Value);
+            Assert.AreEqual(12, root.Children[0].Children[0].Siblings[1].Value);
+        }
+    }
+}
